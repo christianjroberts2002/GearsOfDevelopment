0e313712adf1dc688154c3ecb418679d1e2d392c
christianjroberts2002 <140271461+christianjroberts2002@users.noreply.github.com>
2025-01-23 00:24:13 -0700

    4

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
 
 public class MouseManager : MonoBehaviour
 {
+    public LayerMask layerMask;
+
     private Camera Camera;
+
+    [SerializeField] List<GameObject> preVizWall;
 
     [SerializeField] GameObject preVizGO;
     [SerializeField] GameObject finalGO;
@@ -47,6 +52,8 @@ public class MouseManager : MonoBehaviour
                 break;
             case HouseBuilding.MouseUp:
                 testGO = finalGO;
+                //if(preVizWall.Count > 0)
+                    //DestroyPreVizBlocks(preVizWall);
                 //PlaceBuilding();
                 break;
             case HouseBuilding.None:
@@ -74,9 +81,10 @@ public class MouseManager : MonoBehaviour
 
     private void ClickSetStartTile()
     {
+        
         RaycastHit raycastHit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out raycastHit, 100f))
+        if (Physics.Raycast(ray, out raycastHit, 1000f, layerMask))
         {
             if (raycastHit.transform != null)
             {
@@ -180,7 +188,7 @@ public class MouseManager : MonoBehaviour
 
         RaycastHit raycastHit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out raycastHit, 100f))
+        if (Physics.Raycast(ray, out raycastHit, 1000f, layerMask))
         {
             if (raycastHit.transform != null)
             {
@@ -188,17 +196,22 @@ public class MouseManager : MonoBehaviour
                 //Debug.Log(raycastHit.transform.gameObject.name);
                 GridPosition hitGridPosition = raycastHit.transform.GetComponent<GridPosition>();
                 GridPosition.TilePos tilePose = hitGridPosition.GetTilePos();
+                
                 endTile = tilePose;
                 
                 
                 //Debug.Log(tilePose.X + "," + tilePose.Y);
-                if (endTile.X == startTile.X || endTile.Y == startTile.Y)
+                if (endTile.X == startTile.X && endTile.Y == startTile.Y)
                 {
                     return;
                 }
 
+                if (Mathf.Abs(endTile.X - startTile.X) <= 1 || Mathf.Abs(endTile.Y - startTile.Y) <=1)
+                {
+                    return;
+                }
                 //Testing
-                
+
                 CreateWalls(GetSelectedTiles(startTile, endTile));
                 //Vector3 newGOSpawn = new Vector3(raycastHit.transform.position.x, raycastHit.transform.position.y + 1.5f, raycastHit.transform.position.z);
                 //Instantiate(testGO, newGOSpawn, Quaternion.identity);
@@ -276,19 +289,19 @@ public class MouseManager : MonoBehaviour
         GridPosition.TilePos[,] selectedTiles = new GridPosition.TilePos[Convert.ToInt32(MathF.Abs(startTile.X - endTile.X) + 1),Convert.ToInt32(MathF.Abs(startTile.Y - endTile.Y) + 1)];
 
         int arrayLengthX = selectedTiles.GetLength(0) - 1;
-        int arrayLengthZ = selectedTiles.GetLength(0) - 1;
+        int arrayLengthZ = selectedTiles.GetLength(1) - 1;
 
         
 
         //selectedTiles[0,0] = startTile;
-        //Debug.Log(arrayLengthX + "," + arrayLengthZ);
+        Debug.Log(arrayLengthX + "," + arrayLengthZ);
         //selectedTiles[arrayLengthX, arrayLengthZ] = endTile;
         int startx = Convert.ToInt32(startTile.X);
         int starty = Convert.ToInt32(startTile.Y);
         
         Debug.Log(startx + "," + starty);
 
-      
+
 
 
 
@@ -297,20 +310,20 @@ public class MouseManager : MonoBehaviour
             startx = Convert.ToInt32(startTile.X);
             starty = Convert.ToInt32(startTile.Y);
         }
-        if(startTile.X > endTile.X && startTile.Y > endTile.Y)
+        if (startTile.X > endTile.X && startTile.Y > endTile.Y)
         {
             startx = Convert.ToInt32(endTile.X);
             starty = Convert.ToInt32(endTile.Y);
         }
 
-        if (startTile.X < endTile.X &&  startTile.Y > endTile.Y )
+        if (startTile.X < endTile.X && startTile.Y > endTile.Y)
         {
             startx = Convert.ToInt32(startTile.X);
             starty = Convert.ToInt32(endTile.Y);
 
-                
+
         }
-        if(startTile.X > endTile.X && startTile.Y < endTile.Y )
+        if (startTile.X > endTile.X && startTile.Y < endTile.Y)
         {
             startx = Convert.ToInt32(endTile.X);
             starty = Convert.ToInt32(startTile.Y);
@@ -320,32 +333,35 @@ public class MouseManager : MonoBehaviour
         int ypos = starty;
         
         Debug.Log(xpos + "," + ypos);
+        Debug.Log(endTile.X + "," + endTile.Y);
 
-        if(selectedTiles.GetLength(0) !> 2 && selectedTiles.GetLength(0)! > 2)
+        if (Mathf.Abs(startTile.X - endTile.X) <= 1 || Mathf.Abs(startTile.Y - endTile.Y) <= 1)
         {
             return null;
         }
 
 
-        for (int i = 0; i <= arrayLengthX; i++)
+        for (int j = 0; j <= arrayLengthZ; j++)
         {
-                
-            //Debug.Log(gridSystem.GetTileInArray(xpos, ypos).X + ","  + gridSystem.GetTileInArray(xpos, ypos).Y);
-            //Debug.Log(gridSystem.GetTileInArray(xpos, ypos).X);
-            selectedTiles[0, i] = gridSystem.GetTileInArray(xpos, ypos);
-            selectedTiles[arrayLengthX, i] = gridSystem.GetTileInArray(xpos + arrayLengthX, ypos);
+            selectedTiles[0,j] = gridSystem.GetTileInArray(xpos, ypos);
+            selectedTiles[j, arrayLengthX] = gridSystem.GetTileInArray(xpos + arrayLengthX, ypos);
             ypos++;
 
         }
         ypos = starty;
-        for (int j = 1; j <= arrayLengthZ; j++)
+        for (int i = 1; i <= arrayLengthZ - 1; i++)
         {
-            selectedTiles[j, 0] = gridSystem.GetTileInArray(xpos + 1, ypos);
-            selectedTiles[j, arrayLengthX] = gridSystem.GetTileInArray(xpos + 1, ypos + arrayLengthX);
             xpos++;
+            Debug.Log(gridSystem.GetTileInArray(xpos, ypos).X + ","  + gridSystem.GetTileInArray(xpos, ypos).Y);
+            //Debug.Log(gridSystem.GetTileInArray(xpos, ypos).X);
+            Debug.Log(i);
+            selectedTiles[i,0] = gridSystem.GetTileInArray(xpos, ypos);
+            selectedTiles[arrayLengthZ, i] = gridSystem.GetTileInArray(xpos + arrayLengthX, ypos);
+            
 
         }
-
+        xpos = starty;
+        
 
         for (int i = 0; i <= arrayLengthX; i++)
         {
@@ -370,7 +386,8 @@ public class MouseManager : MonoBehaviour
 
     private void CreateWalls(GridPosition.TilePos[,] selectedTiles)
     {
-        
+        preVizWall = new List<GameObject>();
+
         if(selectedTiles == null)
         {
             return;
@@ -379,7 +396,7 @@ public class MouseManager : MonoBehaviour
         int arrayLengthX = selectedTiles.GetLength(0) - 1;
         int arrayLengthZ = selectedTiles.GetLength(1) - 1;
 
-        if(arrayLengthX !> 1 ||  arrayLengthZ !> 1)
+        if(arrayLengthX !> 2 ||  arrayLengthZ !> 2)
         {
             return;
         }
@@ -389,8 +406,10 @@ public class MouseManager : MonoBehaviour
         {
             for (int j = 0; j <= arrayLengthZ; j++)
             {
-                Vector3 newGOSpawn = new Vector3(selectedTiles[i,j].X * 1.5f, 1.5f, selectedTiles[i,j].Y * 1.5f);
-                Instantiate(testGO, newGOSpawn, Quaternion.identity);
+                Vector3 newGOSpawn = new Vector3(selectedTiles[i,j].Y * 1.5f, 1.5f, selectedTiles[i,j].X * 1.5f);
+                GameObject newBlock = Instantiate(testGO, newGOSpawn, Quaternion.identity);
+                preVizWall.Add(newBlock);
+
 
             }
         }
@@ -412,6 +431,12 @@ public class MouseManager : MonoBehaviour
 
             }
         }
+    }
+
+    private void DestroyPreVizBlocks(List<GameObject> goList)
+    {
+        foreach (GameObject go in goList)
+            Destroy(go);
     }
     
 }
